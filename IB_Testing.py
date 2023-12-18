6ec0ff5899cf0dc3862aeece9b8dcc8c1b06deeb
paulnguyen5634 <paulnguyen@ufl.edu>
2023-12-18 10:26:30 -0500

    Added tick data retrieval

--- IB_Testing.py
+++ IB_Testing.py
@@ -5,19 +5,19 @@ import pandas as pd
 ib = IB()
 # Connecting to localhost, 7496 located in the TWS api settings under 'Socket port'
 # 7497 for paper trading
-ib.connect('127.0.0.1', 7496, clientId=1)
+ib.connect('127.0.0.1', 7497, clientId=1)
 
 #contract = Forex('EURUSD')
 
 # For stocks
 # Parameters: ('symbol', 'SMART' (IB intuitively searches for which exchange the stock is being traded), 'Currency')
-symbol = input('Input Symbol: ')
-timeFrame = int(input('Timeframe: '))
+#symbol = input('Input Symbol: ')
+#timeFrame = int(input('Timeframe: '))
 stock = Stock('AMD', 'SMART', 'USD')
 
 bars = ib.reqHistoricalData(
-    stock, endDateTime='', durationStr='1 D',
-    barSizeSetting='1 min', whatToShow='MIDPOINT', useRTH=True)
+    stock, endDateTime='', durationStr='30 D',
+    barSizeSetting='15 mins', whatToShow='MIDPOINT', useRTH=True)
 
 # convert to pandas dataframe (pandas needs to be installed):
 df = util.df(bars)
---
+++ TickData.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from ib_insync import IB, util, Stock
+
+# Create an instance of the IB class
+ib = IB()
+
+# Connect to the IB Gateway or TWS application
+ib.connect('localhost', 7497, clientId=1)  # Adjust host and port as needed
+
+# Request historical tick data
+contract = Stock('AAPL', 'SMART', 'USD')  # Define the stock contract
+# Note, we c
+ticks = ib.reqHistoricalTicks(contract, startDateTime='20231215 09:30:03', endDateTime='20231215 09:30:04',
+                              numberOfTicks=1000, whatToShow='TRADES', useRth=True)
+
+# Convert tick data to a DataFrame
+df = util.df(ticks)
+size = df['size'].sum()
+print('Size of purchases:', size, 'shares')
+print(df)
